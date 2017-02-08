d41d9bb2c23fed50786f1829da9d98b7002e9fc4
Phan Ngoc <lequidon.1993@gmail.com>
2017-02-08 11:10:36 +0700

    will pagi for show topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -14,6 +14,8 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.includes({:questions => [:topics, :user, :answers => [:user, {:comments => [:actions, :user]}]]}).find(params[:id])
+    @questions = @topic.questions.paginate(:page => params[:page], :per_page => 2)
+
     @countQuestion = @topic.questions.count
     @numberPeopleFollow = Topic.numberFollow @topic.id
     @numberAnswerInTopic = Topic.numberAnwserInTopic @topic.id
