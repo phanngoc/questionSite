cbce4b92201b166a97e6e7e42797210fc37e84e4
Phan Ngoc <lequidon.1993@gmail.com>
2017-02-08 13:53:58 +0700

    add validate for active record user in admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,11 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   enum role: [:admin, :user, :moderator]
 
-  scope :follow_topic, -> (topic_id) {
-
-  }
+  validates :name, presence: true, length: {maximum: 255}
+  validates :story, length: {maximum: 255}
+  validates :email, presence: true, length: {maximum: 255}
+  validates :role, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: I18n.t("flash.user.email")
 
   def self.is_follow_user(user_id, current_user_id)
     query = Action.where(["user_id = ? and actionable_type = ? and actionable_id = ? and type_act = ?",
