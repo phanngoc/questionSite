600d3699d127d323804f54230a4684e67aff13fd
Phan Ngoc <lequidon.1993@gmail.com>
2017-02-26 09:33:03 +0700

    fix admincontroller indent

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,18 +1,18 @@
 class AdminController < ApplicationController
-	layout "admin"
+  layout "admin"
   respond_to :json, :html
   before_action :verify_admin
-	authorize_resource class: false
+  authorize_resource class: false
 
-	def verify_admin
-		redirect_to root_url unless current_user.admin?
-	end
+  def verify_admin
+    redirect_to root_url unless current_user.admin?
+  end
 
-	private
-	
-	def set_admin_ability
-		if admin_signed_in?
-			@current_ability ||= Ability.new current_admin
-		end
-	end
+  private
+
+  def set_admin_ability
+    if admin_signed_in?
+      @current_ability ||= Ability.new current_admin
+    end
+  end
 end
